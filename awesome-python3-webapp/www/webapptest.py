f142e3f6eb4ac8c6fd80dbeb0baa864e2669d039
sunnyshore <shaodeyang@hotmail.com>
2016-07-27 00:30:00 +0800

    Initial commit

---
+++ awesome-python3-webapp/www/webapptest.py
@@ -0,0 +1,17 @@
+import sys
+import orm
+import asyncio
+from models import User, Blog, Comment
+
+async def test(loop):
+    await orm.create_pool(loop=loop, user='www-data', password='www-data', db='awesome')
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    await u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
+if loop.is_closed():
+    sys.exit(0)
