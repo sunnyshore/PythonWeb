42f59a1c2cdacac115a8960b747ba7ae22ef3f60
Shao <shaodeyang@hotmail.com>
2016-08-02 11:13:41 +0800

    add manage_blogs.html and handler

--- awesome-python3-webapp/www/handlers.py
+++ awesome-python3-webapp/www/handlers.py
@@ -150,3 +150,20 @@ def cookie2user(cookie_str):
     except Exception as e:
         logging.exception(e)
         return None
+
+@get('/api/blogs')
+def api_blogs(*, page='1'):
+    page_index = get_page_index(page)
+    num = yield from Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, blogs=())
+    blogs = yield from Blog.findAll(orderBy='created_at_desc', limit=(p.offset, p.limit))
+    return dict(page=p, blogs=blogs)
+
+@get('/manage/blogs')
+def manage_blogs(*, page='1'):
+    return {
+        '__template__': 'manage_blogs.html',
+        'page_index': get_page_index(page)
+    }
